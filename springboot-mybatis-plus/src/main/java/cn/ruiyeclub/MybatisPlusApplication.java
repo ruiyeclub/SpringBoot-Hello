d5fe3e693e55b0ad333df323355fb4859b08a707
chenrui <185048761@qq.com>
2020-07-17 19:57:12 +0800

    init

---
+++ springboot-mybatis-plus/src/main/java/cn/ruiyeclub/MybatisPlusApplication.java
@@ -0,0 +1,17 @@
+package cn.ruiyeclub;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ray。
+ * @date 2020/7/17 19:46
+ */
+@SpringBootApplication
+@MapperScan(basePackages = {"cn.ruiyeclub.mapper"})
+public class MybatisPlusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+}

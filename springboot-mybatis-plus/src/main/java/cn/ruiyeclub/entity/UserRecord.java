7bf44ebbb08fcad7c51b0e1effc70c7e1b90c65c
chenrui <185048761@qq.com>
2020-07-22 14:09:11 +0800

    mybatis-plus未提交部分

---
+++ springboot-mybatis-plus/src/main/java/cn/ruiyeclub/entity/UserRecord.java
@@ -0,0 +1,53 @@
+package cn.ruiyeclub.entity;
+
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+
+/**
+ * (UserRecord)表实体类
+ *
+ * @author Ray。
+ * @date 2020-07-22 14:06:49
+ */
+public class UserRecord extends Model<UserRecord> {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String email;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ springboot-mybatis-plus/src/main/java/cn/ruiyeclub/service/UserRecordService.java
@@ -0,0 +1,14 @@
+package cn.ruiyeclub.service;
+
+import cn.ruiyeclub.entity.UserRecord;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * (UserRecord)表服务接口
+ *
+ * @author Ray。
+ * @date 2020-07-22 14:06:52
+ */
+public interface UserRecordService extends IService<UserRecord> {
+
+}

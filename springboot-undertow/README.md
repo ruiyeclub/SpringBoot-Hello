42ecdd79aa01a06fb132d5e7d101741012e4142f
Raychen <ruiyeclub@foxmail.com>
2020-08-12 18:54:12 +0800

    使用Undertow服务器代替Tomcat

---
+++ springboot-undertow/README.md
@@ -0,0 +1,2 @@
+SpringBoot使用Undertow代替tomcat
+https://www.jianshu.com/p/558f4504d591
---
+++ springboot-undertow/src/main/java/cn/ruiyeclub/UndertowApplication.java
@@ -0,0 +1,16 @@
+package cn.ruiyeclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Ray。
+ * @date 2020/8/7 19:56
+ */
+@SpringBootApplication
+public class UndertowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UndertowApplication.class, args);
+    }
+}

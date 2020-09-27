c46966a8fe2a41478a34929267d62233c97edc24
ruiyeclub <ruiyeclub@foxmail.com>
2020-09-27 15:37:00 +0800

    TODO：整合rabbitMQ消息队列

---
+++ springboot-rabbitmq/src/main/java/cn/ruiyeclub/RabbitMQApplication.java
@@ -0,0 +1,15 @@
+package cn.ruiyeclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Ray。
+ * @Date: 2020/9/27
+ */
+@SpringBootApplication
+public class RabbitMQApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMQApplication.class,args);
+    }
+}

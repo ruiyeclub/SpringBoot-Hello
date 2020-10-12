621875acca6f89df47d32282a54cbb0bd7f9e2b0
ruiyeclub <ruiyeclub@foxmail.com>
2020-10-12 11:15:53 +0800

    整合rabbitMQ消息队列

---
+++ springboot-rabbitmq/README.md
@@ -0,0 +1,2 @@
+SpringBoot 整合 rabbitmq  
+https://www.jianshu.com/p/9a1569755419
--- springboot-rabbitmq/src/main/java/cn/ruiyeclub/RabbitMQApplication.java
+++ springboot-rabbitmq/src/main/java/cn/ruiyeclub/RabbitMQApplication.java
@@ -1,5 +1,6 @@
 package cn.ruiyeclub;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,9 +8,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Author: Ray。
  * @Date: 2020/9/27
  */
+/**
+ * 自动配置
+ * 1. RabbitAutoConfiguration
+ * 2. 自动配置了连接工厂ConnectionFactory
+ * 3. RabbitProperties 封装了RabbitMQ的配置
+ * 4. RabbitTemplate : 给RabbitMQ发送和接受消息
+ * 5. AmqpAdmin : RabbitMQ系统管理功能组件
+ * 6. @EnableRabbit + @RabbitListener
+ */
+@EnableRabbit
 @SpringBootApplication
 public class RabbitMQApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(RabbitMQApplication.class,args);
+        SpringApplication.run(RabbitMQApplication.class, args);
     }
 }

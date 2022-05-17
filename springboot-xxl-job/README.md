786d40c90d3a0fac238981ed00119ce74d486edd
ruiyeclub <ruiyeclub@foxmail.com>
2022-05-17 17:22:54 +0800

    :bowtie:add: springboot-xxl-job

---
+++ springboot-xxl-job/README.md
@@ -0,0 +1,2 @@
+三分钟搞定 XXL-JOB 分布式任务调度平台（ps：搭建该项目需要先运行xxl-job）
+https://mp.weixin.qq.com/s/a7dbQD_bfEcsJokZ37tkEQ
---
+++ springboot-xxl-job/src/main/java/cn/ruiyeclub/XxlJobApplication.java
@@ -0,0 +1,15 @@
+package cn.ruiyeclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Ray。
+ * @Date: 2022/5/17
+ */
+@SpringBootApplication
+public class XxlJobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobApplication.class, args);
+    }
+}

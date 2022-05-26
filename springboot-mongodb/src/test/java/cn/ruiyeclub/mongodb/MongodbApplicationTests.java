8ed598111b40d53a7f60207bb612a1d0bd545f17
ruiyeclub <ruiyeclub@foxmail.com>
2022-05-26 11:01:32 +0800

    feat: pom version

--- springboot-mongodb/src/test/java/cn/ruiyeclub/mongodb/MongodbApplicationTests.java
+++ springboot-mongodb/src/test/java/cn/ruiyeclub/mongodb/MongodbApplicationTests.java
@@ -1,25 +1,19 @@
 package cn.ruiyeclub.mongodb;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.test.context.junit4.SpringRunner;
+
 import javax.annotation.Resource;
 import java.util.List;
+
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
 public class MongodbApplicationTests {
@@ -42,8 +36,8 @@ public class MongodbApplicationTests {
     }
 
     @Test
-    public void find(){
-        List<Demo> demos=mongoTemplate.findAll(Demo.class);
+    public void find() {
+        List<Demo> demos = mongoTemplate.findAll(Demo.class);
         System.out.println(demos.toString());
 
         //可以直接根据id查询
@@ -54,24 +48,24 @@ public class MongodbApplicationTests {
         /**
          * 根据条件查询 可以查询多个数据
          */
-        Query query=new Query(Criteria.where("name").is("李白"));
+        Query query = new Query(Criteria.where("name").is("李白"));
         List<Demo> demos1 = mongoTemplate.find(query, Demo.class);
         System.out.println(demos1.toString());
 
     }
 
     @Test
-    public void update(){
+    public void update() {
 //        mongoTemplate.updateFirst(query(where("name").is("我是凌康")), Update.update("sex","123"),Demo.class);
-        mongoTemplate.updateFirst(query(where("id").is(2)),Update.update("name","是李白"),Demo.class);
+        mongoTemplate.updateFirst(query(where("id").is(2)), Update.update("name", "是李白"), Demo.class);
     }
 
     /**
      * 删除只能根据主键删除
      */
     @Test
-    public void delete(){
-        Demo demo=new Demo();
+    public void delete() {
+        Demo demo = new Demo();
 //        demo.setName("李白");
         demo.setId(1);
         mongoTemplate.remove(demo);

c10b7f74d324301800c032e2e9bd38ab526a07bb
liutian <435757679@qq.com>
2022-08-29 14:37:27 +0800

    add springboot-es

---
+++ springboot-es/src/main/java/com/ttlikeu/Main.java
@@ -0,0 +1,11 @@
+package com.ttlikeu;
+
+/**
+ * @Author: tiantianlikeU。
+ * @Date: 2022年08月29日10:34:45
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
